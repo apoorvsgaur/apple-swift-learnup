18cd7ba126b4e793a372c2e65a8b721870703466
System Administrator <root@pal-nat184-074-165.itap.purdue.edu>
2015-10-15 23:55:22 -0400

    First iOS app made

---
+++ Scavenger Hunt/Scavenger Hunt/AddViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scavenger Hunt
+//
+//  Created by Apoorv Sanjay Gaur on 10/15/15.
+//  Copyright (c) 2015 Apoorv Sanjay Gaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Scavenger Hunt/Scavenger Hunt/ScanvengerHuntItem.swift
@@ -0,0 +1,9 @@
+//
+//  ScanvengerHuntItem.swift
+//  Scavenger Hunt
+//
+//  Created by Apoorv Sanjay Gaur on 10/15/15.
+//  Copyright (c) 2015 Apoorv Sanjay Gaur. All rights reserved.
+//
+
+import Foundation
